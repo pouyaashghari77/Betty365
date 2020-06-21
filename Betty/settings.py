e052dce8f1441c638aeb99d77b72aeb0e8e3468c
Saman Saghafian <saman.saghafian@eskano.com>
2020-06-21 19:31:36 +0430

    Static settings

--- Betty/settings.py
+++ Betty/settings.py
@@ -48,7 +48,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    # 'django.contrib.staticfiles',
+    'django.contrib.staticfiles',
     'django_extensions',
     'import_export',
     'rest_framework',
@@ -153,6 +153,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         # 'knox.auth.TokenAuthentication',
